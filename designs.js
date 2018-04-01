138190e23bfc69836b47bec25f0aa1f0995dcc2f
johngoya <john.s.goya@gmail.com>
2018-04-01 12:05:01 -0700

    Update designs.js

--- designs.js
+++ designs.js
@@ -4,7 +4,7 @@
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
-$('#submit').on('click', function(){
+$('#submit').on("click", function(){
   $('#submit').remove();         
 // Your code goes here!
 
